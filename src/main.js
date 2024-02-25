1813ac2edceefc868aa0d3d975e83e9b71d633ed
Aleks.Po <Poleschuk.A@citilink.ru>
2024-02-26 02:04:00 +0300

    feat(tests): integrate new test to menu

--- src/main.js
+++ src/main.js
@@ -1,18 +1,34 @@
+// @ts-nocheck
 import { Telegraf } from 'telegraf';
-import { message } from 'telegraf/filters';
-
 import config from 'config';
+
+import {
+    runAllTests,
+    runAuthTest,
+    runSwitchProfileTest,
+} from './cypress/index.js'
+
+import { menu } from './lib/index.js';
 
 const bot = new Telegraf(config.get("TELEGRAM_TOKEN"), {
     handlerTimeout: Infinity,
 });
 
-bot.command('start', (ctx) => {
-    ctx.reply('Hi bro! Send ur command');
+bot.command('start', async (ctx) => {
+    await ctx.reply('Привет!');
 });
 
-bot.on(message('text'), (ctx) => {
-    ctx.reply('?');
+bot.command('menu', menu.getMainMenu);
+
+bot.command('help', (ctx) => {
+    ctx.reply('use /menu command');
 });
+
+bot.action('all_test', runAllTests);
+bot.action('auth_test', runAuthTest);
+bot.action('switch_test', runSwitchProfileTest);
 
 bot.launch();
+
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
