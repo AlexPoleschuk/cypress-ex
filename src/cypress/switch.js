f9fb3b0a03529655a2dd8d2e64c887f4fd2d5fbd
Aleks.Po <Poleschuk.A@citilink.ru>
2024-02-26 14:26:17 +0300

    feat(utils): add test factory, opt

--- src/cypress/switch.js
+++ src/cypress/switch.js
@@ -1,43 +1,14 @@
-// @ts-nocheck
-import cypress from "cypress";
-import config from '../../cypress.config.js';
-import { utils } from '../lib/index.js';
+import createTestRunFn from './testFactory.js';
 
 const runSwitchProfileTest = async (ctx) => {
     if (ctx) {
         try {
-            ctx.reply('Тест переключения профиля');
-
-            const interval = utils.showProcess(ctx);
-
-            const results = await cypress.run({
-                ...config,
-                browser: 'chrome',
+            await createTestRunFn({
+                ctx,
                 spec: 'cypress/e2e/switchProfile.cy.js',
-            }).then(results => {
-                if (results?.status === 'failed') {
-                    return null;
-                } else {
-                    return results;
-                }
-            }).catch((e) => {
-                console.error(e);
+                entity: 'switchProfile',
+                description: 'Тест переключения профиля',
             });
-
-            const chatId = utils.getChatId(ctx);
-            const htmlAnswer = utils.getResultsHtml(ctx, results);
-
-            await ctx.telegram.sendMessage(
-                chatId,
-                htmlAnswer,
-                { parse_mode: 'html' },
-            );
-
-            await utils.hideProcess(ctx, interval);
-
-            if (results?.totalFailed > 0) {
-                await utils.sendMediaFailtureResults(ctx, results, `switchProfile`);
-            }
         } catch (e) {
             console.error(e);
         }
